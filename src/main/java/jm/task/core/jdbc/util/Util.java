4a1ee0d97b8aa87d842c3bede4086336f5b7f0de
bermik <102170118+bermik@users.noreply.github.com>
2022-03-24 18:02:29 +0900

    completing 1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.*;
 import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private static final String URL = "jdbc:mysql://localhost:3306/mdbtest";
+    private static final String URL = "jdbc:mysql://localhost:3306/mdbtest?useSSL=false";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
     private static Connection connection;
+    private static SessionFactory sessionFactory;
 
     public Util() {
     }
@@ -19,5 +27,29 @@ public class Util {
             connection.setAutoCommit(false);
         }
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory() throws SQLException {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.URL, URL);
+            settings.put(Environment.USER, USER);
+            settings.put(Environment.PASS, PASSWORD);
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+            configuration.setProperties(settings);
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        }
+        return sessionFactory;
     }
 }
